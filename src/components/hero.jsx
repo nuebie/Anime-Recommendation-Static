b73342378e326166382dac52913ba919f36b3d15
Emman <manugasemmanuell@gmail.com>
2023-07-14 22:37:26 +0800

    second commit

---
+++ src/components/hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./navbar";
+import heroimage from '../images/sampleimg.jpg'
+
+export default function Hero(){
+    return(
+        <div className="heroSection">
+            <div className="heroContainer">
+                <Navbar/>
+                <div className="heroMain">
+                    <div className="left">
+                        <h1>Looking For a New Anime to Watch?</h1>
+                        <p>Rate a few animes that you have watched and we will recommend some animes that you might enjoy using Artificial Intelligence (AI).</p>
+                        <button className="heroButton" id="trybutton">Try Now</button>
+                    </div>
+                    <div className="right">
+                        <img src={heroimage} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
