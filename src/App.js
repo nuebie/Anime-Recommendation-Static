4d13476cd596c69930ad8bbcd8dbdf0363bda3e4
Emman <manugasemmanuell@gmail.com>
2023-07-15 20:58:30 +0800

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {createContext, useContext, useState} from 'react';
 import Note from './components/note';
 import Hero from './components/hero';
 import WatchedAnime from './components/watchedanime';
@@ -5,7 +6,10 @@ import Recommendations from './components/recommendations';
 import './App.css';
 import { Element, scroller } from "react-scroll";
 
+const context = createContext();
+
 function App() {
+  const [isLoading, setIsLoading] = useState(0);
   
   function scrollToWatched(){
     scroller.scrollTo("watched", {
@@ -14,6 +18,10 @@ function App() {
   }
 
   function scrollToRecommendations(){
+    setIsLoading(current => current===2? 1: current+1);
+    setTimeout(() => {
+      setIsLoading(current=> current+1)
+    }, 3000);
     scroller.scrollTo("recommendations", {
       smooth: true,
     });
@@ -21,18 +29,21 @@ function App() {
   
   return (
     <div className="App">
-      <Note/>
-      <Element name='hero'>
-        <Hero scrollToWatched={scrollToWatched}/>
-      </Element>
-      <Element name='watched'>
-        <WatchedAnime scrollToRecommendations={scrollToRecommendations}/>
-      </Element>
-      <Element name='recommendations'>
-        <Recommendations/>
-      </Element>
+      <context.Provider value={{isLoading,setIsLoading}}>
+        <Note/>
+        <Element name='hero'>
+          <Hero scrollToWatched={scrollToWatched}/>
+        </Element>
+        <Element name='watched'>
+          <WatchedAnime scrollToRecommendations={scrollToRecommendations}/>
+        </Element>
+        <Element name='recommendations'>
+          <Recommendations/>
+        </Element>
+      </context.Provider>
     </div>
   );
 }
 
 export default App;
+export {context};
--- src/components/recommendations.jsx
+++ src/components/recommendations.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, {useContext} from "react";
 import AnimeRecomCard from "./animerecomcard";
+import Loader from "./loader";
+import { context } from "../App";
 
 export default function Recommendations(){
     const animeRecommendations = ["Fullmetal Alchemist: Brotherhood", "Black Clover", "Yu Yu Hakusho", "Haikyuu!!", "Sword Art Online", "JoJo's Bizarre Adventure", "Tokyo Ghoul", "The Seven Deadly Sins", "Gintama", "Assassination Classroom"];
-
+    const {isLoading} = useContext(context);
     return(
         <div className="recommendationSection">
             <div className="recommendationcontainer">
                 <div className="recommendationList">
                     <h1>Recommendations</h1>
                     <div className="recommendationListInner">
-                        <div className="animeRecommendationWrapper">
-                            {animeRecommendations.map(title=> (
+                        <div className={`animeRecommendationWrapper ${isLoading === 1? "currLoading":''} ${isLoading === 2? "finLoading":''}`}>
+                            {isLoading === 1 && <Loader/>}
+                            {isLoading === 2 && animeRecommendations.map(title=> (
                                 <AnimeRecomCard title={title}/>
                             ))}
                         </div>
